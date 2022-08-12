03d5e2afba370ab2f0d82fd963709cc0fd00ae57
Yang Li <york0516@gmail.com>
2022-08-12 18:08:59 +1000

    Refactor the routes to use explicit request methods

--- ghapapp/routes/web.php
+++ ghapapp/routes/web.php
@@ -14,30 +14,29 @@
 /**
  * Home and search pages
  */
-//Route::any('/', function () {return redirect('');});
-Route::any('/home', function () {
+Route::get('/home', function () {
     return redirect('');
 })->name('home');
-Route::any('', 'HomeController@index')->name('index');
-Route::any('file', 'GazetteerController@searchFromFile')->name('searchFromFile'); //search from file
-Route::any('kmlpolygonsearch', 'GazetteerController@searchFromKmlPolygon')->name('searchFromKmlPolygon'); //search from file
-Route::any('places/{id?}', 'GazetteerController@search')->name('places'); //shows places with optional id, if no id is given it uses all results before applying filters
-Route::any('search', 'GazetteerController@search')->name('search')->middleware('checkmaxpaging');
-Route::any('maxpaging', 'GazetteerController@maxPagingMessage')->name('maxPagingMessage');
-Route::any('maxpagingredirect', 'GazetteerController@maxPagingRedirect')->name('maxPagingRedirect');
+Route::get('', 'HomeController@index')->name('index');
+Route::post('file', 'GazetteerController@searchFromFile')->name('searchFromFile'); //search from file
+Route::post('kmlpolygonsearch', 'GazetteerController@searchFromKmlPolygon')->name('searchFromKmlPolygon'); //search from file
+Route::get('places/{id?}', 'GazetteerController@search')->name('places'); //shows places with optional id, if no id is given it uses all results before applying filters
+Route::get('search', 'GazetteerController@search')->name('search')->middleware('checkmaxpaging');
+Route::get('maxpaging', 'GazetteerController@maxPagingMessage')->name('maxPagingMessage');
+Route::get('maxpagingredirect', 'GazetteerController@maxPagingRedirect')->name('maxPagingRedirect');
 Route::post('bulkfileparser', 'GazetteerController@bulkFileParser');
 
 /**
  * Public dataset pages
  */
-Route::any('publicdatasets', 'DatasetController@viewPublicDatasets')->name('publicdatasets');
-Route::any('publicdatasets/{id}', 'DatasetController@viewPublicDataset')->name('publicdataset');
-Route::any('publicdatasets/{id}/kml', 'DatasetController@viewPublicKML')->name('viewpublicdatasetkml');
-Route::any('publicdatasets/{id}/kml/download', 'DatasetController@downloadPublicKML')->name('downloadpublicdatasetkml');
-Route::any('publicdatasets/{id}/json', 'DatasetController@viewPublicJSON')->name('viewpublicdatasetjson');
-Route::any('publicdatasets/{id}/json/download', 'DatasetController@downloadPublicJSON')->name('downloadpublicdatasetjson');
-Route::any('publicdatasets/{id}/csv', 'DatasetController@viewPublicCSV')->name('viewpublicdatasetcsv');
-Route::any('publicdatasets/{id}/csv/download', 'DatasetController@downloadPublicCSV')->name('downloadpublicdatasetcsv');
+Route::get('publicdatasets', 'DatasetController@viewPublicDatasets')->name('publicdatasets');
+Route::get('publicdatasets/{id}', 'DatasetController@viewPublicDataset')->name('publicdataset');
+Route::get('publicdatasets/{id}/kml', 'DatasetController@viewPublicKML')->name('viewpublicdatasetkml');
+Route::get('publicdatasets/{id}/kml/download', 'DatasetController@downloadPublicKML')->name('downloadpublicdatasetkml');
+Route::get('publicdatasets/{id}/json', 'DatasetController@viewPublicJSON')->name('viewpublicdatasetjson');
+Route::get('publicdatasets/{id}/json/download', 'DatasetController@downloadPublicJSON')->name('downloadpublicdatasetjson');
+Route::get('publicdatasets/{id}/csv', 'DatasetController@viewPublicCSV')->name('viewpublicdatasetcsv');
+Route::get('publicdatasets/{id}/csv/download', 'DatasetController@downloadPublicCSV')->name('downloadpublicdatasetcsv');
 
 /**
  * Public collection pages.
@@ -50,29 +49,28 @@ Route::get('publiccollections/{id}/json', 'CollectionController@viewPublicJson')
  * User Pages.
  */
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::any('myprofile', 'User\UserController@userProfile')->name('myProfile');
-    Route::any('myprofile/mydatasets', 'User\UserController@userDatasets')->name('myDatasets'); //Only let users view own dataset
-    Route::any('myprofile/mysearches', 'User\UserController@userSavedSearches')->name('mySearches');
-    Route::any('myprofile/mysearches/delete', 'User\UserController@deleteUserSavedSearches');
-    Route::any('myprofile/mydatasets/newdataset', 'User\UserController@newDatasetPage');
-    Route::any('myprofile/mydatasets/newdataset/create', 'User\UserController@createNewDataset');
-    Route::any('myprofile/mydatasets/{id}', 'User\UserController@userViewDataset'); //Only let users view own dataset
-    Route::any('myprofile/mydatasets/{id}/collaborators', 'User\UserController@userEditCollaborators');
-    Route::any('myprofile/mydatasets/{id}/collaborators/destroysharelink', 'User\UserController@userDestroyShareLink');
+    Route::get('myprofile', 'User\UserController@userProfile')->name('myProfile');
+    Route::get('myprofile/mydatasets', 'User\UserController@userDatasets')->name('myDatasets'); //Only let users view own dataset
+    Route::get('myprofile/mysearches', 'User\UserController@userSavedSearches')->name('mySearches');
+    Route::post('myprofile/mysearches/delete', 'User\UserController@deleteUserSavedSearches');
+    Route::get('myprofile/mydatasets/newdataset', 'User\UserController@newDatasetPage');
+    Route::post('myprofile/mydatasets/newdataset/create', 'User\UserController@createNewDataset');
+    Route::get('myprofile/mydatasets/{id}', 'User\UserController@userViewDataset'); //Only let users view own dataset
+    Route::get('myprofile/mydatasets/{id}/collaborators', 'User\UserController@userEditCollaborators');
     Route::post('bulkadddataitem', 'User\UserController@bulkAddDataItem'); //not ajax as it is too much data
-    Route::any('myprofile/mydatasets/{id}/edit', 'User\UserController@userEditDataset');
-    Route::any('myprofile/edit', 'User\UserController@editUserPage')->name('editUserPage');
-    Route::any('myprofile/edit/info', 'User\UserController@editUserInfo')->name('editUserInfo');
-    Route::any('myprofile/edit/password', 'User\UserController@editUserPassword')->name('editUserPassword');
-    Route::any('myprofile/edit/email', 'User\UserController@editUserEmail')->name('editUserEmail');
-    Route::any('myprofile/mydatasets/join/{link?}', 'AjaxController@ajaxjoindataset'); //Join a dataset by link
+    Route::post('myprofile/mydatasets/{id}/edit', 'User\UserController@userEditDataset');
+    Route::get('myprofile/edit', 'User\UserController@editUserPage')->name('editUserPage');
+    Route::post('myprofile/edit/info', 'User\UserController@editUserInfo')->name('editUserInfo');
+    Route::post('myprofile/edit/password', 'User\UserController@editUserPassword')->name('editUserPassword');
+    Route::post('myprofile/edit/email', 'User\UserController@editUserEmail')->name('editUserEmail');
+    Route::post('myprofile/mydatasets/join/{link?}', 'AjaxController@ajaxjoindataset'); //Join a dataset by link
 
-    Route::any('myprofile/mydatasets/{id}/kml', 'DatasetController@viewPrivateKML')->name('viewdatasetkml');
-    Route::any('myprofile/mydatasets/{id}/kml/download', 'DatasetController@downloadPrivateKML')->name('downloaddatasetkml');
-    Route::any('myprofile/mydatasets/{id}/json', 'DatasetController@viewPrivateJSON')->name('viewdatasetjson');
-    Route::any('myprofile/mydatasets/{id}/json/download', 'DatasetController@downloadPrivateJSON')->name('downloaddatasetjson');
-    Route::any('myprofile/mydatasets/{id}/csv', 'DatasetController@viewPrivateCSV')->name('viewdatasetcsv');
-    Route::any('myprofile/mydatasets/{id}/csv/download', 'DatasetController@downloadPrivateCSV')->name('downloaddatasetcsv');
+    Route::get('myprofile/mydatasets/{id}/kml', 'DatasetController@viewPrivateKML')->name('viewdatasetkml');
+    Route::get('myprofile/mydatasets/{id}/kml/download', 'DatasetController@downloadPrivateKML')->name('downloaddatasetkml');
+    Route::get('myprofile/mydatasets/{id}/json', 'DatasetController@viewPrivateJSON')->name('viewdatasetjson');
+    Route::get('myprofile/mydatasets/{id}/json/download', 'DatasetController@downloadPrivateJSON')->name('downloaddatasetjson');
+    Route::get('myprofile/mydatasets/{id}/csv', 'DatasetController@viewPrivateCSV')->name('viewdatasetcsv');
+    Route::get('myprofile/mydatasets/{id}/csv/download', 'DatasetController@downloadPrivateCSV')->name('downloaddatasetcsv');
 });
 
 /**
@@ -86,25 +84,18 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::post('myprofile/mycollections/{id}/edit', 'CollectionController@editCollection');
 });
 
-
 /**
  * Admin pages
  * The Admin Controller passes through 'auth' and 'verified' middleware for all functions AND checks user is admin
  * Each method manually checks for ADMIN/SUPER_ADMIN itself, will display 403 if not of sufficient role
  */
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::any('admin', 'Auth\AdminController@adminHome'); //Only let ADMIN or SUPER_ADMIN access this page
-    Route::any('admin/users', 'Auth\AdminController@userManagement'); //Only let SUPER_ADMIN access this page
-    Route::any('admin/users/{email}', 'Auth\AdminController@viewUser'); //Only let  SUPER_ADMIN access this page
-    Route::any('admin/users/{email}/activateDeactivateUser', 'Auth\AdminController@activateDeactivateUser'); //Only let SUPER_ADMIN access this page
-    Route::any('admin/users/{email}/updateUserRole', 'Auth\AdminController@updateUserRole'); //Only let SUPER_ADMIN access this page
+    Route::get('admin', 'Auth\AdminController@adminHome'); //Only let ADMIN or SUPER_ADMIN access this page
+    Route::get('admin/users', 'Auth\AdminController@userManagement'); //Only let SUPER_ADMIN access this page
+    Route::get('admin/users/{id}', 'Auth\AdminController@viewUser'); //Only let  SUPER_ADMIN access this page
+    Route::post('admin/users/{email}/activateDeactivateUser', 'Auth\AdminController@activateDeactivateUser'); //Only let SUPER_ADMIN access this page
+    Route::post('admin/users/{email}/updateUserRole', 'Auth\AdminController@updateUserRole'); //Only let SUPER_ADMIN access this page
 });
-
-/**
- * Misc Functions
- */
-Route::any('places/download/', 'GazetteerController@getDownload');
-
 
 /**
  * AJAX functions
@@ -146,14 +137,11 @@ Route::middleware(['auth', 'verified'])->group(function () { //must be logged in
     Route::get('ajax/collections/{collection_id}/datasets/addable/{dataset_id}/info', 'CollectionController@ajaxGetDatasetInfo');
 });
 
-
 /**
  * Authentication routes? (unsure what this is specifically)
  */
-//Auth::routes();
 Auth::routes(['verify' => true]);
 Route::get('verify', 'Auth\VerificationController@showPage');
-
 
 /**
  * Output gaz as lpf
