845b94145209862af8f505955995f37a8883add4
Yang Li <york0516@gmail.com>
2022-11-29 18:43:02 +1100

    Some wording changes to the email verification message.

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
+                    {{ __('Please check your email for a verification link. Allow a few minutes for it to arrive and check your spam or clutter folder.') }}
                     {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
                 </div>
             </div>
