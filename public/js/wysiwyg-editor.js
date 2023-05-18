75ad63b4f822dd614cedc42b301ba45ae0d9d0a3
Yang Li <york0516@gmail.com>
2023-05-18 17:03:36 +1000

    Fix the non-editable link text input of Wysiwyg editor

--- public/js/wysiwyg-editor.js
+++ public/js/wysiwyg-editor.js
@@ -9,4 +9,11 @@ $(document).ready( function () {
         toolbar: 'bold italic link | h2 h3 h4 h5 | bullist',
         valid_elements: 'h2,h3,h4,h5,p,strong/b,em/i,a[href|target|title],ul,li,br'
     });
+
+    // To solve the issue of text fields in TinyMCE popup window are not editable when TinyMCE editor is embedded in
+    // bootstrap modal. Ref: https://github.com/tinymce/tinymce/issues/782
+    $(document).on('focusin', function(e) {
+        if ($(e.target).closest(".tox-textfield").length)
+            e.stopImmediatePropagation();
+    });
 });
