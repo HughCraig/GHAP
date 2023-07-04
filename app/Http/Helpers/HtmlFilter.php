5fda68a404a082729ef68623b660a5729d9b7798
Yang Li <york0516@gmail.com>
2023-07-05 09:54:24 +1000

    Adapt TLCMap views configurations

--- app/Http/Helpers/HtmlFilter.php
+++ app/Http/Helpers/HtmlFilter.php
@@ -15,7 +15,7 @@ class HtmlFilter
     public static function simple($raw)
     {
         if (!empty($raw)) {
-            return strip_tags($raw, '<h2><h3><h4><h5><p><strong><em><a><ul><li><br>');
+            return strip_tags($raw, '<h2><h3><h4><h5><p><strong><em><a><ul><li><br><table><tr><td><th><tbody><thead><tfoot>');
         }
         return '';
     }
