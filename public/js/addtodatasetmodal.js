e429b3f02c0658bddae89a7b786d8e4f8e856d9e
Mufeng Niu <mufeng@Mufengs-MacBook-Pro.local>
2023-09-08 20:00:04 +0800

    Issue #189. Fix auto-suggest for terms and lg a in dataitem edit and add modal

--- public/js/addtodatasetmodal.js
+++ public/js/addtodatasetmodal.js
@@ -1,4 +1,32 @@
 $(document).ready(function () {
-    $('#addDateStartDiv').datepicker({format: 'yyyy-mm-dd', todayBtn: true, forceParse: false, keyboardNavigation: false});
-    $('#addDateEndDiv').datepicker({format: 'yyyy-mm-dd', todayBtn: true, forceParse: false, keyboardNavigation: false});
+    $("#addDateStartDiv").datepicker({
+        format: "yyyy-mm-dd",
+        todayBtn: true,
+        forceParse: false,
+        keyboardNavigation: false,
+    });
+    $("#addDateEndDiv").datepicker({
+        format: "yyyy-mm-dd",
+        todayBtn: true,
+        forceParse: false,
+        keyboardNavigation: false,
+    });
+
+    //LGA autocomplete.
+    $("#addlga").autocomplete({
+        source: function (request, response) {
+            var results = $.ui.autocomplete.filter(lgas, request.term);
+            response(results.slice(0, 20)); //return only 20 results
+        },
+    });
+    $("#addlga").autocomplete("option", "appendTo", ".eventInsForm");
+
+    //feature_term autocomplete.
+    $("#addfeatureterm").autocomplete({
+        source: function (request, response) {
+            var results = $.ui.autocomplete.filter(feature_terms, request.term);
+            response(results.slice(0, 20)); //return only 20 results
+        },
+    });
+    $("#addfeatureterm").autocomplete("option", "appendTo", ".eventInsForm");
 });
