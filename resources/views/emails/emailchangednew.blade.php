a7b43487e0102ddfe46654b9dcdb5937ce142809
Mufeng Niu <mufeng.niu@gmail.com>
2023-11-13 15:58:22 +1100

    Issue #181. Add TLCmap branding for email

--- resources/views/emails/emailchangednew.blade.php
+++ resources/views/emails/emailchangednew.blade.php
@@ -1,11 +1,49 @@
-Hello!
-<br>
-<br>
-This is an automated email to notify you that your TLCMap account has now been changed to this address.
-<br>
-<br>
-If you did not perform this action no further actions are required and you may ignore this email.
-<br>
-<br>
-<br>
-This is an automated message from <a href="tlcmap.org">TLCMap</a>
+<table>
+    <tbody>
+        <tr>
+            <td style="box-sizing:border-box;padding:25px 0;text-align:center">
+                <a href="https://ghap.tlcmap.org/" style="box-sizing:border-box;color:#bbbfc3;font-size:19px;font-weight:bold;text-decoration:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://u33125758.ct.sendgrid.net/ls/click?upn%3DfolzJiUXAQ97NwNE8JlD-2BNyy6R0HDAdAnjYloKzpZvcPUVs5RnirMOM0M1DM47zyjro-_F7j-2BNjtkZJWXJQwkuQqNUzm4O0qe9CGMd5KVkZB9Ogv1Ik7Es9JgtRCETKYxxW64TaJnNpwljVje-2FluaBw1tGAJVMJT45e5ylHwTA0iCUU3z0oaZgrP3ADCSUOSg3HWxDY9NuV7GSVWy35zlePKfBx6X7FqBjaRds-2FTT0N2MxecGxgcMk6SNtqvF3rAhzqlthbf5Zd9P0xknas7QP6Xowg-3D-3D&amp;source=gmail&amp;ust=1699937201761000&amp;usg=AOvVaw3d_M3akSAt3S7LimtcA-HI">
+                    TLCMap Gazetteer of Historical Australian Places
+                </a>
+            </td>
+        </tr>
+
+        <tr>
+            <td width="100%" cellpadding="0" cellspacing="0" style="border-bottom:1px solid #edeff2;border-top:1px solid #edeff2;margin:0;padding:0;width:100%">
+                <table align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="margin:0 auto;padding:0;width:570px">
+                    <tbody>
+                        <tr>
+                            <td style="padding:35px">
+                                Hello!
+                                <br>
+                                <br>
+                                This is an automated email to notify you that your TLCMap account has now been changed to this address.
+                                <br>
+                                <br>
+                                If you did not perform this action no further actions are required and you may ignore this email.
+                                <br>
+                                <br>
+                                <br>
+                                This is an automated message from <a href="tlcmap.org">TLCMap</a>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <table align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="margin:0 auto;padding:0;text-align:center;width:570px">
+                    <tbody>
+                        <tr>
+                            <td align="center" style="padding:35px">
+                                <p style="line-height:1.5em;margin-top:0;color:#aeaeae;font-size:12px;text-align:center">© 2023 TLCMap Gazetteer of Historical Australian Places. All rights reserved.</p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
